5c7dbbd946ced8df66dc5399eb4a5e64baa5e226
Alexander Sommerhuber <alexander.sommerhuber@students.fh-hagenberg.at>
2015-05-02 16:36:33 +0200

    Issue 85: Anzeigefehler
    Ergebnis der Signaturvalidierung wurde in den Nachrichtenbody
    eingefügt. Dies führte zu Problemen bei erneuter Validierung.
    Als Lösung wurde eine eigene Form für die Statusanzeige
    eingefügt (verificationBar).

---
+++ OutlookPrivacyPlugin/verificationBar.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace OutlookPrivacyPlugin
+{
+    [System.ComponentModel.ToolboxItemAttribute(false)]
+    partial class verificationBar : Microsoft.Office.Tools.Outlook.FormRegionBase
+    {
+        public verificationBar(Microsoft.Office.Interop.Outlook.FormRegion formRegion)
+            : base(Globals.Factory, formRegion)
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.status = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // status
+            // 
+            this.status.BackColor = System.Drawing.Color.Silver;
+            this.status.Enabled = false;
+            this.status.Location = new System.Drawing.Point(3, 3);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(332, 23);
+            this.status.TabIndex = 0;
+            this.status.Text = "Status";
+            this.status.UseVisualStyleBackColor = false;
+            // 
+            // verificationBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.status);
+            this.Name = "verificationBar";
+            this.Size = new System.Drawing.Size(338, 31);
+            this.FormRegionShowing += new System.EventHandler(this.FormRegion1_FormRegionShowing);
+            this.FormRegionClosed += new System.EventHandler(this.FormRegion1_FormRegionClosed);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        #region Vom Formularbereich-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private static void InitializeManifest(Microsoft.Office.Tools.Outlook.FormRegionManifest manifest, Microsoft.Office.Tools.Outlook.Factory factory)
+        {
+            manifest.FormRegionName = "Status";
+            manifest.FormRegionType = Microsoft.Office.Tools.Outlook.FormRegionType.Adjoining;
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button status;
+
+
+        public partial class FormRegion1Factory : Microsoft.Office.Tools.Outlook.IFormRegionFactory
+        {
+            public event Microsoft.Office.Tools.Outlook.FormRegionInitializingEventHandler FormRegionInitializing;
+
+            private Microsoft.Office.Tools.Outlook.FormRegionManifest _Manifest;
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            public FormRegion1Factory()
+            {
+                this._Manifest = Globals.Factory.CreateFormRegionManifest();
+                verificationBar.InitializeManifest(this._Manifest, Globals.Factory);
+                this.FormRegionInitializing += new Microsoft.Office.Tools.Outlook.FormRegionInitializingEventHandler(this.FormRegion1Factory_FormRegionInitializing);
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            public Microsoft.Office.Tools.Outlook.FormRegionManifest Manifest
+            {
+                get
+                {
+                    return this._Manifest;
+                }
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            Microsoft.Office.Tools.Outlook.IFormRegion Microsoft.Office.Tools.Outlook.IFormRegionFactory.CreateFormRegion(Microsoft.Office.Interop.Outlook.FormRegion formRegion)
+            {
+                verificationBar form = new verificationBar(formRegion);
+                form.Factory = this;
+                return form;
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            byte[] Microsoft.Office.Tools.Outlook.IFormRegionFactory.GetFormRegionStorage(object outlookItem, Microsoft.Office.Interop.Outlook.OlFormRegionMode formRegionMode, Microsoft.Office.Interop.Outlook.OlFormRegionSize formRegionSize)
+            {
+                throw new System.NotSupportedException();
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            bool Microsoft.Office.Tools.Outlook.IFormRegionFactory.IsDisplayedForItem(object outlookItem, Microsoft.Office.Interop.Outlook.OlFormRegionMode formRegionMode, Microsoft.Office.Interop.Outlook.OlFormRegionSize formRegionSize)
+            {
+                if (this.FormRegionInitializing != null)
+                {
+                    Microsoft.Office.Tools.Outlook.FormRegionInitializingEventArgs cancelArgs = Globals.Factory.CreateFormRegionInitializingEventArgs(outlookItem, formRegionMode, formRegionSize, false);
+                    this.FormRegionInitializing(this, cancelArgs);
+                    return !cancelArgs.Cancel;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            Microsoft.Office.Tools.Outlook.FormRegionKindConstants Microsoft.Office.Tools.Outlook.IFormRegionFactory.Kind
+            {
+                get
+                {
+                    return Microsoft.Office.Tools.Outlook.FormRegionKindConstants.WindowsForms;
+                }
+            }
+        }
+    }
+
+    partial class WindowFormRegionCollection
+    {
+        internal verificationBar FormRegion1
+        {
+            get
+            {
+                foreach (var item in this)
+                {
+                    if (item.GetType() == typeof(verificationBar))
+                        return (verificationBar)item;
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ OutlookPrivacyPlugin/verificationBar.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Office = Microsoft.Office.Core;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Color = System.Drawing.Color;
+
+namespace OutlookPrivacyPlugin
+{
+    partial class verificationBar
+    {
+        #region Formularbereichsfactory
+
+        [Microsoft.Office.Tools.Outlook.FormRegionMessageClass(Microsoft.Office.Tools.Outlook.FormRegionMessageClassAttribute.Note)]
+        [Microsoft.Office.Tools.Outlook.FormRegionName("OutlookPrivacyPlugin.FormRegion1")]
+        public partial class FormRegion1Factory
+        {
+            // Tritt ein, bevor der Formularbereich initialisiert wird.
+            // Um die Anzeige des Formularbereichs zu verhindern, legen Sie e.Cancel auf True fest.
+            // Verwenden Sie e.OutlookItem, um einen Verweis auf das aktuelle Outlook-Element abzurufen.
+            private void FormRegion1Factory_FormRegionInitializing(object sender, Microsoft.Office.Tools.Outlook.FormRegionInitializingEventArgs e)
+            {
+            }
+        }
+        
+        #endregion
+
+        // Tritt ein, bevor der Formularbereich angezeigt wird.
+        // Verwenden Sie this.OutlookItem, um einen Verweis auf das aktuelle Outlook-Element abzurufen.
+        // Verwenden Sie this.OutlookFormRegion, um einen Verweis auf den Formularbereich abzurufen.
+        private void FormRegion1_FormRegionShowing(object sender, System.EventArgs e)
+        {
+        }
+
+        // Tritt ein, wenn der Formularbereich geschlossen wird.
+        // Verwenden Sie this.OutlookItem, um einen Verweis auf das aktuelle Outlook-Element abzurufen.
+        // Verwenden Sie this.OutlookFormRegion, um einen Verweis auf den Formularbereich abzurufen.
+        private void FormRegion1_FormRegionClosed(object sender, System.EventArgs e)
+        {
+        }
+
+        private void status_Click_1(object sender, EventArgs e)
+        {
+            this.status.BackColor = Color.Crimson;
+            this.status.Text = "verified";
+        }
+
+        public void status_valid()
+        {
+            status.Text = "Valid Signature!";
+            status.BackColor = Color.PaleGreen;
+        }
+
+        public void status_invalid()
+        {
+            this.Enabled = true;
+            status.Text = "Invalid Signature!";
+            status.BackColor = Color.Red;
+        }
+    }
+}
